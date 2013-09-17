b745d84b8c8385fe5f1975927c76eb4eb0d74f6e
Colin Seymour <lildood@gmail.com>
2013-09-17 15:01:23 +0100

    Removed comment from top of file as this works now

--- antd/strava.py
+++ antd/strava.py
@@ -1,23 +1,20 @@
 # coding=utf-8
-# 
-# NOTE: This doesn't work yet as the login steps need work
-# 
-# 
+#
 # Copyright (c) 2013, Colin Seymour.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-# 
+#
 #   1. Redistributions of source code must retain the above copyright
 #      notice, this list of conditions and the following disclaimer.
-# 
+#
 #   2. Redistributions in binary form must reproduce the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer in the documentation and/or other materials
 #      provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS
 # ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -54,7 +51,7 @@ class Strava(plugin.Plugin):
 
     authenticity_token = None
 
-    def __init__(self):     
+    def __init__(self):
         import poster.streaminghttp
         cookies = cookielib.CookieJar()
         cookie_handler = urllib2.HTTPCookieProcessor(cookies)
