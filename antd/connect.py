a6b07f00ab39d0a12de21aa99a628ffa02c04be9
Colin Seymour <lildood@gmail.com>
2013-09-17 15:21:07 +0100

    Removed Strava email as it is now in its own file

--- antd/connect.py
+++ antd/connect.py
@@ -1,18 +1,18 @@
 # Copyright (c) 2012, Braiden Kindt.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-# 
+#
 #   1. Redistributions of source code must retain the above copyright
 #      notice, this list of conditions and the following disclaimer.
-# 
+#
 #   2. Redistributions in binary form must reproduce the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer in the documentation and/or other materials
 #      provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS
 # ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -112,7 +112,7 @@ class GarminConnect(plugin.Plugin):
         elif username != self.username:
             _log.warning("Username mismatch, probably OK, if upload fails check user/pass. %s != %s" % (username, self.username))
         self.logged_in = True
-    
+
     def upload(self, format, file_name):
         import poster.encode
         with open(file_name) as file:
@@ -121,71 +121,8 @@ class GarminConnect(plugin.Plugin):
                 "data": file,
             }
             data, headers = poster.encode.multipart_encode(upload_dict)
-            _log.info("Uploading %s to Garmin Connect.", file_name) 
+            _log.info("Uploading %s to Garmin Connect.", file_name)
             request = urllib2.Request("http://connect.garmin.com/proxy/upload-service-1.1/json/upload/.%s" % format, data, headers)
             self.opener.open(request)
-        
-class StravaEmail(plugin.Plugin):
-
-    server = None
-    smtp_server = None
-    smtp_port = None
-    smtp_username = None
-    smtp_password = None
-
-    logged_in = False
-
-    def __init__(self):
-        from smtplib import SMTP
-        self.server = SMTP()
-        pass
-
-    def data_available(self, device_sn, format, files):
-        if format not in ("tcx"): return files
-        result = []
-        try:
-            for file in files:
-                self.login()
-                self.upload(format, file)
-                result.append(file)
-            self.logout()
-        except Exception:
-            _log.warning("Failed to upload to Strava.", exc_info=True)
-        finally:
-            return result
-
-    def logout(self):
-        self.server.close()
-
-    def login(self):
-        if self.logged_in: return
-        self.server.connect(self.smtp_server, self.smtp_port)
-        self.server.ehlo()
-        if self.smtp_password:
-            self.server.starttls()
-            self.server.ehlo()
-            self.server.login(self.smtp_username, self.smtp_password)
-        self.logged_in = True
-    
-    def upload(self, format, file_name):
-        from email.mime.base import MIMEBase
-        from email.mime.multipart import MIMEMultipart
-        import datetime
-        from email import encoders
-        outer = MIMEMultipart()
-        outer['Subject'] = 'Garmin Data Upload from %s' % datetime.date.today()
-        outer['To' ] = 'upload@strava.com'
-        outer['From' ] = self.smtp_username
-        outer.preamble = 'You will not see this in a MIME-aware mail reader.\n'
-        with open(file_name, 'rb') as fp:
-            msg = MIMEBase('application', 'octet-stream')
-            msg.set_payload(fp.read())
-        encoders.encode_base64(msg)
-        msg.add_header('Content-Disposition', 'attachment', filename=file_name)
-        outer.attach(msg)
-        self.server.sendmail(self.smtp_username, 'upload@strava.com', outer.as_string())
-
-class InvalidLogin(Exception): pass
-
 
 # vim: ts=4 sts=4 et
